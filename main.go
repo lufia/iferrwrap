6f353e06ab81b2023185d522be03180be6d3d2a7
lufia <lufia@lufia.org>
2018-10-05 03:41:10 +0900

    generate wrapper code

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"go/ast"
 	"go/build"
+	"go/format"
 	"go/importer"
 	"go/parser"
 	"go/token"
 	"go/types"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"text/template"
 )
 
 type Methoder interface {
@@ -26,12 +31,86 @@ func main() {
 	//typeName := "DB"
 	pkgName := "./testdata/pkg"
 	typeName := "T"
-	if err := codegen(pkgName, typeName); err != nil {
+	var buf bytes.Buffer
+	if err := codegen(&buf, pkgName, typeName); err != nil {
 		log.Fatalln(err)
 	}
+	code, err := format.Source(buf.Bytes())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%s", code)
 }
 
-func codegen(pkgName, typeName string) error {
+var codeTemplate = strings.TrimSpace(`
+package {{.Package}}
+
+{{$type := printf "err%s" .Name}}
+type {{$type}} struct {
+	Val *{{.Name}}
+	Err error
+}
+
+{{range .Methods}}
+func (p *{{$type}}) {{.Name}}{{.Params}} {
+	if p.Err != nil {
+		return
+	}
+	{{with .Returns}}{{.}} = {{end}}p.Val.{{.Name}}({{.Args}})
+}
+{{end}}
+`)
+
+type methodParam struct {
+	Name    string
+	Params  *types.Tuple
+	Results *types.Tuple
+}
+
+func (m *methodParam) Args() string {
+	args := make([]string, m.Params.Len())
+	for i := 0; i < m.Params.Len(); i++ {
+		args[i] = m.Params.At(i).Name()
+	}
+	return strings.Join(args, ", ")
+}
+
+func (m *methodParam) Returns() string {
+	n := m.Results.Len()
+	vars := make([]string, n)
+	if m.isTrailingErr() {
+		n--
+	}
+	for i := 0; i < n; i++ {
+		vars[i] = "_"
+	}
+	if m.isTrailingErr() {
+		vars[n] = "p.Err"
+	}
+	return strings.Join(vars, ", ")
+}
+
+func (m *methodParam) lastResult() types.Type {
+	if m.Results == nil || m.Results.Len() == 0 {
+		return nil
+	}
+	return m.Results.At(m.Results.Len() - 1).Type()
+}
+
+func (m *methodParam) isTrailingErr() bool {
+	last := m.lastResult()
+	return isErrorType(last)
+}
+
+type typeParam struct {
+	Package string
+	Name    string
+	Methods []*methodParam
+}
+
+func codegen(w io.Writer, pkgName, typeName string) error {
+	tmpl := template.Must(template.New("code").Parse(codeTemplate))
+
 	ctx := build.Default
 	//ctx.BuildTags = []string{}
 	pkg, err := ctx.Import(pkgName, ".", 0)
@@ -42,6 +121,9 @@ func codegen(pkgName, typeName string) error {
 	if err != nil {
 		return err
 	}
+	var param typeParam
+	param.Package = "xxx" // current package name
+	param.Name = typeName
 	meths, err := t.Methods()
 	if err != nil {
 		return err
@@ -51,16 +133,13 @@ func codegen(pkgName, typeName string) error {
 		if !ok {
 			return fmt.Errorf("%v: don't have signature?", m)
 		}
-		rv := sig.Results()
-		if rv == nil || rv.Len() == 0 {
-			log.Println(m, ": void")
-			continue
-		}
-		if !isErrorType(rv.At(rv.Len() - 1).Type()) {
-		}
-		log.Println(m.Name(), sig, ok)
+		param.Methods = append(param.Methods, &methodParam{
+			Name:    m.Name(),
+			Params:  sig.Params(),
+			Results: sig.Results(),
+		})
 	}
-	return nil
+	return tmpl.Execute(w, &param)
 }
 
 func isErrorType(t types.Type) bool {
@@ -124,7 +203,7 @@ func lookupType(dir, pkgName, typeName string) (*Type, error) {
 		}
 		return &Type{pkg, obj.Type()}, nil
 	}
-	return nil, fmt.Errorf("type %s.%s is not exist", pkg.Name, typeName)
+	return nil, fmt.Errorf("type %s.%s is not exist", pkg.Name(), typeName)
 }
 
 func (t *Type) Methods() ([]*types.Func, error) {
--- testdata/pkg/f.go
+++ testdata/pkg/f.go
@@ -3,15 +3,15 @@ package pkg
 func (t *T) PtrMethod1() {
 }
 
-func (t T) Method2() error {
+func (t T) Method2(s string) error {
 	return nil
 }
 
-func (t *T) PtrMethod3() (int, error) {
+func (t *T) PtrMethod3(int) (int, error) {
 	return 0, nil
 }
 
-func (t T) Method4() (string, int, error) {
+func (t T) Method4(v interface{}, err error) (string, int, error) {
 	return "", 0, nil
 }
 
